f7d82dda685120ba534cc8239f69584668214e00
Alessandro De Waele <alessandro.dewaele@student.hogent.be>
2025-09-11 15:01:38 +0200

    test

--- frontend/src/pages/registerPage.vue
+++ frontend/src/pages/registerPage.vue
@@ -19,7 +19,7 @@ const registerUser = async () => {
 </script>
 
 <template>
-  register page dikke boktor eeeeee testee
+  register page dikke boktor eeeeee testee eeeee
   <form @submit.prevent="submit">
     <input v-model="username" placeholder="Username" />
     <input type="password" v-model="password" placeholder="Password" />
