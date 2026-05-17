dd5555926a9cfee3340fdc5325c79a6dfd77ce4c
Alessandro De Waele <alessandro.dewaele@student.hogent.be>
2026-05-17 12:13:28 +0200

    Deployable

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,7 +5,7 @@ const itemRoutes = require("./routes/itemRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
